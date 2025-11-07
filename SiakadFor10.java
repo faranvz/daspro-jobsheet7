338f3240dc2b2f43b6227fbcf2012c9debf9cce5
faranvz <s4tgo77@gmail.com>
2025-11-07 10:01:32 +0700

    Modifikasi For

---
+++ SiakadFor10.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SiakadFor10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai;
+        double tertinggi = 0;
+        double terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ":");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi =  nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+            if (nilai >= 60) {
+                lulus++;
+            } if (nilai < 60) {
+                tidakLulus++;
+            }
+        }
+        System.out.println("Nilai tertinggi: " +tertinggi);
+        System.out.println("Nilai terendah: " +terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " +lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " +tidakLulus);
+    }
+}
